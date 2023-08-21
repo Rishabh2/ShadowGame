0c790f29ade19684b1fcc87281ff586ea55b2b61
Rishabh Bhasin <rishabh.bhasin.33@gmail.com>
2023-08-20 20:36:00 -0700

    Character table with file support

---
+++ script.js
@@ -0,0 +1,39 @@
+var file = document.getElementById("fileIn"); // Add file button
+var characterTable = document.getElementById("characterTable");
+
+file.onchange = function (e) {
+    var tgt = e.target || window.event.srcElement;
+    var files = tgt.files;
+
+    // FileReader support
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+            var fileResult = fr.result;
+
+            var row = characterTable.insertRow();
+            var cName = row.insertCell();
+            var cThumb = row.insertCell();
+            var cHor = row.insertCell();
+            var cVer = row.insertCell();
+            var cState = row.insertCell();
+
+            // Populate each cell with the associate elements.
+            cName.innerHTML = fileResult.name;
+            cThumb.innerHTML = "thumb";
+            cHor.innerHTML = "hor";
+            cVer.innerHTML = "ver";
+            cState.innerHTML = "state";
+        }
+        for (var newfile in files) {
+            fr.readAsDataURL(newFile);
+        }
+    }
+
+    // Not supported
+    else {
+        // fallback -- perhaps submit the input to an iframe and temporarily store
+        // them on the server until the user's session ends.
+    }
+
+};
