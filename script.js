5ddfec5d289e37b111c7a2b303668b8a68a8955b
Rishabh Bhasin <rishabh.bhasin.33@gmail.com>
2023-08-21 23:31:10 -0700

    fin typo

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function updateChars(value) {
 
 function processCanvas() {
     const ctx = canvas.getContext('2d');
-    ctx.fillstyle = "#ffffff";
+    ctx.fillStyle = "#ffffff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     sortedChars = [];
     for (let character in characterImages) {
